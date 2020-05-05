c0e072e037da6c5ec00ba046801eef901043cea8
yvanremy <hagenimanayvanremy@gmail.com>
2020-05-05 14:24:03 -0500

    adding account service

---
+++ e-account/src/main/java/com/emarket/EAccountApplication.java
@@ -0,0 +1,13 @@
+package com.emarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EAccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EAccountApplication.class, args);
+	}
+
+}
